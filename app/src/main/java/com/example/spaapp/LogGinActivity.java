33a316bfff4ed92b44bda4cc1868cf023b153ab1
Admin <trung1999>
2022-04-26 23:14:57 +0700

    trung dz

---
+++ app/src/main/java/com/example/spaapp/LogGinActivity.java
@@ -0,0 +1,14 @@
+package com.example.spaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogGinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_gin);
+    }
+}
